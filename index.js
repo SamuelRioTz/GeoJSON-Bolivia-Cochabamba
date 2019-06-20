935873267e1a67a24bbe7b29461968b637f56775
Samuel <samuel@MacBook-Pro-de-Samuel.local>
2019-06-19 22:57:55 -0400

    run script

---
+++ index.js
@@ -0,0 +1,20 @@
+const osmToGeojson = require('osm-public-transport-export')
+
+osmToGeojson({
+    bounds: {
+        south: -17.57727,
+        west: -66.376555,
+        north: -17.276198,
+        east: -65.96397,
+    },
+    outputDir: __dirname + '/out',
+    mapProperties: (tags) => ({
+        ...tags,
+        stroke: '#164154',
+        "stroke-width": 5,
+    }),
+    stopNameSeparator: ' y ',
+    stopNameFallback: 'innominada',
+})
+    .then(data => console.log("done"))
+    .catch(error => console.error(error))

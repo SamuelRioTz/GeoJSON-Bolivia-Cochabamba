e966f4f1d0f14f3bb2644625d8bfa52b14f8442b
Samuel <samuel@MacBook-Pro-de-Samuel.local>
2019-06-20 02:37:49 -0400

    link osm

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ osmToGeojson({
 | State | Id | Name | Ref | From | To |
 | ----- | -- | ---- | --- | ---- | -- |`
         data.log.forEach(element => {
-            out_file += `\n${element.error_log ? "⁉️" : "✅"} | ${element.id} | ${element.name} | ${element.ref} | ${element.from} | ${element.to}`
+            out_file += `\n${element.error_log ? "⁉️" : "✅"} | [${element.id}](https://www.openstreetmap.org/relation/${element.id}) | ${element.name} | ${element.ref} | ${element.from} | ${element.to}`
         });
         // console.log(out_file)
         fs.writeFileSync("README.md", out_file)
